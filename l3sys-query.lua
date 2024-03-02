0d2b01ecebccd686ecc843a77a94d05db2cb93f4
Joseph Wright <joseph@texdev.net>
2024-03-02 19:37:02 +0000

    Initial l3sys-query.lua
    This includes the #! line so it's not forgotten.
    
    Currently no license information: to be agreed yet.

---
+++ l3sys-query.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/env texlua
+
+--[[
+
+File l3sys-query.lua Copyright (C) 2024 The LaTeX Project
+
+-----------------------------------------------------------------------
+
+The development version of the bundle can be found at
+
+   https://github.com/latex3/lsys-query
+
+for those people who are interested.
+
+--]]
